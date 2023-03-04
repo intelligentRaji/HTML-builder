a4a1c6a801fdd83f20578d01a62889fa8fa3f13b
intelligentRaji <sergdim1234@mail.ru>
2023-03-04 02:00:35 +0100

    fix: changes recursive function to promise

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,23 +2,6 @@ const fs = require("fs/promises");
 const path = require("path");
 const { createWriteStream, createReadStream } = require("fs");
 
-async function writeFile(arrayOfFiles, folder, writeStream) {
-  if (arrayOfFiles.length !== 0) {
-    const filePath = path.join(folder, arrayOfFiles[0]);
-    if (path.extname(filePath) === ".css") {
-      const readableStream = createReadStream(filePath, "utf-8");
-      readableStream.on("data", (chunk) => {
-        writeStream.write(chunk, "utf-8");
-      });
-      readableStream.on("end", () => {
-        writeFile(arrayOfFiles.slice(1), folder, writeStream);
-      });
-    } else {
-      writeFile(arrayOfFiles.slice(1), folder, writeStream);
-    }
-  }
-}
-
 async function createBundleCSS(folderName, targetName) {
   const folderPath = path.join(__dirname, folderName);
   const dirPath = path.join(__dirname, "project-dist");
@@ -29,7 +12,20 @@ async function createBundleCSS(folderName, targetName) {
   const writableStream = createWriteStream(targetPath);
 
   const files = await fs.readdir(folderPath);
-  writeFile(files, folderPath, writableStream);
+  for (let file of files) {
+    const filePath = path.join(folderPath, file);
+    if (path.extname(filePath) === ".css") {
+      await new Promise((res, rej) => {
+        const readableStream = createReadStream(filePath, "utf-8");
+        readableStream.on("data", (chunk) => {
+          writableStream.write(chunk, "utf-8");
+        });
+        readableStream.on("end", () => {
+          res();
+        });
+      });
+    }
+  }
 }
 
 createBundleCSS("styles", "bundle.css");
